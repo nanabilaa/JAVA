501e74165af566b1c95590429148fc43c31ac38c
nabila <143019770+nanabilaa@users.noreply.github.com>
2024-10-22 11:13:01 +0700

    latihan

---
+++ oop9/Latihan/Hewan.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laprak;
+
+/**
+ *
+ * @author NABILA
+ */
+abstract class Hewan {
+abstract void suara();
+    void info(){
+        System.out.println("Hewan ini dapat bersuara.");
+    }
+    
+    
+    
+}
---
+++ oop9/Latihan/Main1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laprak;
+/**
+ *
+ * @author NABILA
+ */
+
+public class Main1 {
+    public static void main (String[] args){
+    Hewan anjing = new Anjing();
+    Hewan kucing = new Kucing ();
+    kucing.info();
+    kucing.suara();
+    anjing.info();
+    anjing.suara();
+    }
+    
+}
